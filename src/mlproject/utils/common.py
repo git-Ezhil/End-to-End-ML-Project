65ab501317b5eb93737565b009cb7b75f9feab28
git-Ezhil <ezhil.eete@gmail.com>
2024-04-18 12:33:07 +0530

    utils added

--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -3,8 +3,8 @@ from box.exceptions import BoxValueError
 import yaml 
 from mlproject import logger
 import json
-import joblib
-from ensure import ensure_annotations
+import joblib # type: ignore
+from ensurepip import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
